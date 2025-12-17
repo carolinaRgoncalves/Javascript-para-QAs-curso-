e5103eddced07b1c8703ea0228223e7781dd77e5
carolinaRgoncalves <111509146+carolinaRgoncalves@users.noreply.github.com>
2025-12-17 11:52:59 -0300

    Add files via upload

---
+++ js/basic.js
@@ -0,0 +1,108 @@
+console.log('Olá, Javascript!')
+
+var userName = 'Carolina Rico'
+
+document.getElementById('user-name').innerHTML = userName
+
+//Variaveis//
+
+//var nome = 'Mestre Yoda'
+//var idade = 100
+//var jedi = true
+
+//console.log(typeof nome)
+//console.log(typeof idade)
+//console.log(typeof jedi)
+
+//Operadores Matematicos//
+
+//var n1 = 7
+//var n2 = 2.5
+//var n2 = '5'
+
+//console.log(typeof n1)
+//console.log(typeof n2)
+
+/* + para somar
+   - para subtrair
+   * para multiplicar
+   / para dividir   */
+
+//var total = n1 + parseInt(n2)
+//var total = n1 / n2
+//console.log(total)
+
+//Operadores de comparação//
+
+//var v1 = 5
+//var v2 = '5'
+
+//var resultado = v1 !== v2
+console.log(resultado)
+
+// Igual (==)
+// Retorna verdadeiro se os valores comparados forem iguais.
+//1=='1' //true
+
+// Não igual (!=)
+// Retorna verdadeiro se os valores comparados não forem iguais. Esse operador também pode ser chamado de diferente de.
+//4!=1 //true
+
+// Igual estrito (===)
+// Esse operador é mais severo, só retorna verdadeiro se o valor e o tipo comparados forem iguais.
+//3==='3' //false
+//3===3 //true
+
+// Não igual estrito (!==)
+// Não se engane, esse operador vai retornar verdadeiro se os valores e ou os tipos forem diferentes
+//3!=='3' //true
+//3!==3 //false
+//3!==4 //true
+
+// Maior que (>)
+// Compara se o operador da esquerda é maior que o da direita. Caso seja retorna verdadeiro
+//1>2 //false
+//5>3 //true
+//4>'1' //true
+
+// Maior ou igual que (>=)
+// Compara se o operador da esquerda é maior ou igual ao da direita. Caso seja retorna verdadeiro
+//1>=2 //false
+//5>=3 //true
+//4>='1' //true
+//2>=2 // true
+
+// Menor que (<)
+// Compara se o operador da esquerda é menor que o da direita. Caso seja retorna verdadeiro
+//1<2 //true
+//5<3 //false
+//4<'1' //false    
+
+// Menor ou igual que (<=)
+// Compara se o operador da esquerda é menor ou igual ao da direita. Caso seja retorna verdadeiro
+//1<=2 //true
+//5<=3 //false
+//4<='1' //false
+//2<=2 // true
+
+// function soma(n1, n2) {
+//     console.log(n1 + n2)
+
+// }
+
+// soma(5, 5)
+
+//Windows: Ctrl + K e depois Ctrl + C para comentar, ou Ctrl + K e depois Ctrl + U para descomentar.
+
+// function boasVindas(nome) {
+//     alert(nome + ', sejam bem-vindo(a)')
+// }
+
+// boasVindas (Carolina)
+
+// function soma(n1, n2) {
+//     return n1 + n2
+// }
+
+// var resultado = soma (5,5)
+// console.log(resultado)
